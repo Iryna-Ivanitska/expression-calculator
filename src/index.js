9641e874bc540819789ac850b9578a1e3318256f
Iryna Ivanytska <ira.pechenitsya@gmail.com>
2021-08-22 09:57:09 +0300

    feat: add expressionCalculator function

--- src/index.js
+++ src/index.js
@@ -3,8 +3,25 @@ function eval() {
     return;
 }
 
+
 function expressionCalculator(expr) {
-    // write your solution here
+    const string = expr.split(" ").join('');
+    if (string.search('/0') >= 0) throw new Error('TypeError: Division by zero.');
+        let brackets = string.replace(/[^()]/g, '');
+        let tmp = 0;
+    for (c of brackets) {
+        if (c === '(') tmp++;
+    else if (c === ')' && --tmp < 0) throw new Error('ExpressionError: Brackets must be paired');
+  }
+  if (tmp != 0) throw new Error('ExpressionError: Brackets must be paired');
+    
+        function solve(expr) {
+                return new Function('return ' + expr)()
+        }
+        if (solve(string) === Infinity) {
+            throw new Error('TypeError: Division by zero.');
+        } else return solve(string);
+   
 }
 
 module.exports = {
